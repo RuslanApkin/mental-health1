e6b532312b28de8f636a29d6b17e1ec9ddb9554a
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-28 15:18:54 +0500

    feat: initialized telegraf module

--- telegram-bot/src/modules/bot/bot.module.ts
+++ telegram-bot/src/modules/bot/bot.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
 
-@Module({})
+@Module({
+  imports: [
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        token:
+          process.env.TELEGRAM_TOKEN || configService.get('TELEGRAM_TOKEN'),
+      }),
+    }),
+  ],
+})
 export class BotModule {}

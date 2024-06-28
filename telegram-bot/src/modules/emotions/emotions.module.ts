9244c5f253e1f5a89a35f6be387623907d024e39
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-28 18:58:48 +0500

    reafctor: moved emotion servcies from couchdb to emotions module

--- telegram-bot/src/modules/emotions/emotions.module.ts
+++ telegram-bot/src/modules/emotions/emotions.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { CouchDbModule } from '../couchdb/couchdb.module';
 
-@Module({})
+@Module({ imports: [CouchDbModule] })
 export class EmotionsModule {}
--- telegram-bot/src/modules/emotions/emotions.service.ts
+++ telegram-bot/src/modules/emotions/emotions.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { CouchDbService } from '../couchdb/couchdb.service';
+import { Emotions } from '../couchdb/types';
+import { emotionsCalc } from 'src/utils';
 
 @Injectable()
-export class EmotionsService {}
+export class EmotionsService {
+  constructor(private readonly couchDb: CouchDbService) {}
+
+  public async updateUserEmotionsByChatId(chatId: number, emotions: Emotions) {
+    const users = await this.couchDb.getUserByChatId(chatId);
+    if (users.length === 0) throw new Error('User not found');
+    const user = users[0];
+    user.emotions = user.emotions
+      ? emotionsCalc(user.emotions, emotions)
+      : emotions;
+    const response = await this.couchDb.updateUser(user);
+    return response;
+  }
+
+  public async getEmotionsByChatId(chatId: number): Promise<Emotions> {
+    const users = await this.couchDb.getUserByChatId(chatId);
+    if (users.length === 0) throw new Error('User not found');
+    const user = users[0];
+    return user.emotions;
+  }
+}

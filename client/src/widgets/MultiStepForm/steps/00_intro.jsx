7127c84d3059a9e3f483d0eb80544c726554350e
Denis Beresnev <shanthi.bunch@gmail.com>
2024-06-28 22:14:38 +0300

    feat: intro page

--- client/src/widgets/MultiStepForm/steps/00_intro.jsx
+++ client/src/widgets/MultiStepForm/steps/00_intro.jsx
@@ -1,3 +1,12 @@
+import { useTelegram } from "../../../app/hooks/telegram";
+
 export const IntroStep = () => {
-	return <div>Inro</div>;
+	const { user } = useTelegram();
+
+	return (
+		<>
+			<h1>Hello, {user?.first_name || user?.username || "Friend"}!</h1>
+			<span>Take a short test to assess your emotional state</span>
+		</>
+	);
 };

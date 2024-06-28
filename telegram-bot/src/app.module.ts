8ec151edd853a2feeca9cf90a7da6ef47e36fffe
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-28 15:15:16 +0500

    feat: added configModule

--- telegram-bot/src/app.module.ts
+++ telegram-bot/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `./env/.${process.env.NODE_ENV}.env`,
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}

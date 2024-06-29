2ec4fae3261c1d9b3aa37f4531a85dc80a978db2
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-29 22:47:54 +0500

    feat: added emotions controller

--- telegram-bot/src/modules/emotions/emotions.controller.ts
+++ telegram-bot/src/modules/emotions/emotions.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Emotions } from '../couchdb/types';
+import { EmotionsService } from './emotions.service';
 
 @Controller('emotions')
-export class EmotionsController {}
+export class EmotionsController {
+  constructor(private readonly emotionsServcie: EmotionsService) {}
+
+  @Get(':id')
+  public async getEmotionsByChatId(
+    @Param('id') chatId: number,
+  ): Promise<Emotions> {
+    return await this.emotionsServcie.getEmotionsByChatId(chatId);
+  }
+}
--- telegram-bot/src/modules/emotions/emotions.module.ts
+++ telegram-bot/src/modules/emotions/emotions.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CouchDbModule } from '../couchdb/couchdb.module';
 import { EmotionsService } from './emotions.service';
+import { EmotionsController } from './emotions.controller';
 
 @Module({
   imports: [CouchDbModule],
+  controllers: [EmotionsController],
   providers: [EmotionsService],
   exports: [EmotionsService],
 })

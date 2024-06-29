23ae2d4bce07259cb433f91585ad298dc0fc9f29
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-29 16:58:41 +0500

    refactor: renamed mistral module to model

--- telegram-bot/src/modules/model/model.service.ts
+++ telegram-bot/src/modules/model/model.service.ts
@@ -2,16 +2,10 @@ import { Injectable } from '@nestjs/common';
 //import { pipeline } from '@xenova/transformers';
 
 @Injectable()
-export class MistralService {
+export class ModelService {
   constructor() {}
   public async getResponse() //chat: Array<{ role: string; content: string }>,
   : Promise<string> {
-    //const pipe = await pipeline(
-    //  'text-generation',
-    //  'mistralai/Mistral-7B-Instruct-v0.2',
-    //);
-    //const response = await pipe(chat);
-    //return response[0]['generated_text'][-1]['content'];
     return 'response';
   }
 }

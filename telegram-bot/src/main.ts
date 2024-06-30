f858f8a753c033d8668501e5de1c8de40029879e
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-30 16:50:03 +0500

    fix: fixed port

--- telegram-bot/src/main.ts
+++ telegram-bot/src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
     exposedHeaders: ['Authorization'],
     credentials: true, // Enable credentials (cookies, authorization headers) cross-origin
   });
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();

55bb399e3206d026e607166f1a9ed829872d41eb
RuslanApkin <ruslan-apkin@yandex.ru>
2024-06-30 13:38:36 +0500

    fix: fixed chat to user

--- client/src/app/hooks/telegram.js
+++ client/src/app/hooks/telegram.js
@@ -1,24 +1,26 @@
 export const useTelegram = () => {
-	const {
-		initDataUnsafe,
-		close,
-		ready,
-		setHeaderColor,
-		MainButton,
-		BackButton,
-		themeParams,
-		chat,
-	} = window.Telegram.WebApp;
+  const {
+    initDataUnsafe,
+    close,
+    ready,
+    setHeaderColor,
+    MainButton,
+    BackButton,
+    themeParams,
+    chat,
+    sendData,
+  } = window.Telegram.WebApp;
 
-	setHeaderColor("secondary_bg_color");
+  setHeaderColor("secondary_bg_color");
 
-	return {
-		user: initDataUnsafe?.user,
-		chat,
-		ready,
-		close,
-		MainButton,
-		BackButton,
-		themeParams,
-	};
+  return {
+    user: initDataUnsafe?.user,
+    chat,
+    ready,
+    close,
+    MainButton,
+    BackButton,
+    themeParams,
+    sendData,
+  };
 };
